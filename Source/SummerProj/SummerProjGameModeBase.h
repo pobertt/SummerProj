4becdd4a7a443183cb6ef6c3f41042a9a8c6cfcb
pobertt <79865756+pobertt@users.noreply.github.com>
2024-05-10 16:58:11 +0100

    added files

---
+++ Source/SummerProj/SummerProjGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SummerProjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUMMERPROJ_API ASummerProjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
